4b7d91dc5619fb6f43d3e3a3be1f84ae6f639ed7
Kieran Halliday <kluhini96@users.noreply.github.com>
2017-07-06 13:54:53 -0700

    Day 5
    A simple radio group that tells you which button you pressed

--- src/main/java/com/example/kieranhalliday/practiceapplication/MainActivity.java
+++ src/main/java/com/example/kieranhalliday/practiceapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.kieranhalliday.practiceapplication;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -8,41 +9,34 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
     TextView textView;
-    SeekBar seekBar;
+    RadioGroup radioGroup;
+    RadioButton r1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         textView = (TextView) findViewById(R.id.text);
-
-        seekBar = (SeekBar) findViewById(R.id.seek);
-        seekBar.setOnSeekBarChangeListener(this);
+        radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
+        r1 = (RadioButton) findViewById(R.id.r1);
+        radioGroup.setOnCheckedChangeListener(this);
 
     }
 
     @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        if(progress<=45 && progress>=40){
-            textView.setVisibility(View.VISIBLE);
-            textView.setText(String.valueOf(progress));
-        }else{
-            textView.setVisibility(View.GONE);
-        }
-    }
+    public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
+        int val = Math.abs(r1.getId()-checkedId)+1;
+        textView.setText("You have selected the " + val +" Radio Button");
     }
 }
